7302052ea8965e6c9dd9fbfc3a215e66e4515d8d
Uyen Nguyen <udnguyen@ucsd.edu>
2021-10-23 23:27:53 -0700

    add text file and modified code to open file

--- rosalind/python_village/5_working_with_files.py
+++ rosalind/python_village/5_working_with_files.py
@@ -1,11 +1,11 @@
 # Given: A file containing at most 1000 lines.
 # Return: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
 
-# Make sure the file intended to be opened is in the same directory with the working_with_files.py file
-# Create an empty file named output.txt to write the output data (in the same directory)
+# Make sure the file intended to be opened is in the same working directory with the 5_working_with_files.py file
+# The output.txt file will be created after executing the code
 
 # Use for loop
-f = open('rosalind_ini5.txt', 'r')
+f = open('txt_files/rosalind_ini5.txt', 'r')
 g = open('output.txt', 'w')
 
 i = 1
@@ -19,6 +19,6 @@ g.close()
 # Or
 # A more elegant way
 # [1::2] means print out the lines starting from line #1, with an incrementation of 2
-f = open('rosalind_ini5.txt', 'r')
+f = open('txt_files/rosalind_ini5.txt', 'r')
 g = open('output.txt', 'w')
 g.write(''.join(f.readlines()[1::2]))
