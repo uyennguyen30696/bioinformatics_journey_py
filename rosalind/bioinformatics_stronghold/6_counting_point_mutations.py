392049aa61c773334ee6fa94a49df46f7717c440
Uyen Nguyen <udnguyen@ucsd.edu>
2021-10-28 00:51:28 -0700

    counting point mutation

---
+++ rosalind/bioinformatics_stronghold/6_counting_point_mutations.py
@@ -0,0 +1,14 @@
+# Given two strings s and t of equal length, the Hamming distance between s and t, denoted dH(s,t), is the number of corresponding symbols that differ in s and t. See Figure 2.
+
+# Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+# Return: The Hamming distance dH(s,t).
+
+s = open('txt_files/rosalind_hamm.txt', 'r').readlines()[0]
+t = open('txt_files/rosalind_hamm.txt', 'r').readlines()[1]
+
+count = 0
+for i in range(len(s)):
+    if s[i] != t[i]:
+        count += 1
+        
+print (count)
