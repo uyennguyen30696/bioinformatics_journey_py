3df3d1c4bfb6edecab9e6fcf9d45ea254eae43fc
Uyen Nguyen <udnguyen@ucsd.edu>
2021-11-07 00:04:18 -0700

    translating rna into protein

---
+++ rosalind/bioinformatics_stronghold/8_translating_RNA_into_protein.py
@@ -0,0 +1,38 @@
+# The 20 commonly occurring amino acids are abbreviated by using 20 letters from the English alphabet (all letters except for B, J, O, U, X, and Z). Protein strings are constructed from these 20 symbols. Henceforth, the term genetic string will incorporate protein strings along with DNA strings and RNA strings.
+# The RNA codon table dictates the details regarding the encoding of specific codons into the amino acid alphabet.
+
+# Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
+# Return: The protein string encoded by s.
+
+rnaFile = open('txt_files/rosalind_prot.txt').read()
+proteinFile = open('output', 'w')
+
+rnaCodons = {
+    "UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"Stop", "UAG":"Stop",
+    "UGU":"C", "UGC":"C", "UGA":"Stop", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
+
+def rnaToProtein(rnaFile):
+    protein = ''
+    for i in range(0, len(rnaFile), 3):
+        codon = rnaCodons[rnaFile[i:i+3]]
+        if codon == 'Stop':
+            break
+        protein += codon
+    return protein
+
+proteinFile.write(rnaToProtein(rnaFile))
+print (rnaToProtein(rnaFile))
